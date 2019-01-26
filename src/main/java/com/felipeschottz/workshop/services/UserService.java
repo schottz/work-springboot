e724a6684c9d560c1fea357bce435b237ea73140
Felipe Schottz <schottz@gmail.com>
2019-01-25 22:19:03 -0200

    insercao usuarios

--- src/main/java/com/felipeschottz/workshop/services/UserService.java
+++ src/main/java/com/felipeschottz/workshop/services/UserService.java
@@ -1,12 +1,15 @@
 package com.felipeschottz.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipeschottz.workshop.domain.User;
+import com.felipeschottz.workshop.dto.UserDTO;
 import com.felipeschottz.workshop.repository.UserRepository;
+import com.felipeschottz.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +20,19 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
