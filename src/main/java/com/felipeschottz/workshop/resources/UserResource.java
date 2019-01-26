bf080384049b50f030ea46ced40f93c949481dfd
Felipe Schottz <schottz@gmail.com>
2019-01-26 11:21:45 -0200

    end point return posts

--- src/main/java/com/felipeschottz/workshop/resources/UserResource.java
+++ src/main/java/com/felipeschottz/workshop/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.felipeschottz.workshop.domain.Post;
 import com.felipeschottz.workshop.domain.User;
 import com.felipeschottz.workshop.dto.UserDTO;
 import com.felipeschottz.workshop.services.UserService;
@@ -65,6 +66,14 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)	
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
