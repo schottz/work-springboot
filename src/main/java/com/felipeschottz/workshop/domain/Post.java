e56e3d83e7f976b188e59bc318a270ed659f4995
Felipe Schottz <schottz@gmail.com>
2019-01-29 10:53:11 -0200

    confusão total

--- src/main/java/com/felipeschottz/workshop/domain/Post.java
+++ src/main/java/com/felipeschottz/workshop/domain/Post.java
@@ -1,12 +1,15 @@
 package com.felipeschottz.workshop.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.felipeschottz.workshop.dto.AuthorDTO;
+import com.felipeschottz.workshop.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -61,6 +66,14 @@ public class Post implements Serializable{
 
 	public void setBody(String body) {
 		this.body = body;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
